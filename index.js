84c714c0263a3c7d649417c764c3038768d25436
N'chaka Benjamin <43768096+Nchaka@users.noreply.github.com>
2018-11-01 19:56:55 -0400

    Node Is Required To Run The App
    You now need Node.js to run the application
    
    You can also serve the JSON file using the getJSON callback. For instance do:
    http://localhost:8080/getJSON

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
-var http, html, fileSystem, header, djson;
+var http, html, fileSystem, url, dJSONPath, path;
 
 http = require('http');
-fileSystem = require('fs')
+fileSystem = require('fs');
+url = require('url');
+dJSONPath = '/getJSON'; // The async path/function to the JSON file
 
-header = {'Content-Type': 'text/html'};
+htmlHeader = {'Content-Type': 'text/html'};
+txtHeader = {'Content-Type': 'text/plain'}; 
 
-http.createServer(function(req, res){ 
-	fileSystem.readFile('index.htm', function(error, data){
-		res.writeHead(200, header);
-		res.end(data);
-	});
+http.createServer(function(request, response){ 
+	path = url.parse(request.url).pathname;
+	if(path == dJSONPath){ // Serve the JSON resource via HTTP
+		fileSystem.readFile('dataPointsSum.json', 'UTF8', function(error, dJSON){
+			response.writeHead(200, txtHeader);
+			response.end(dJSON);			
+		});		
+	}
+	else{ // Serve index.htm via HTTP
+		fileSystem.readFile('index.htm', function(error, data){
+			response.writeHead(200, htmlHeader);
+			response.end(data);
+		});
+	}	
 }).listen(8080);
